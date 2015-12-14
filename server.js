fed96926df92e44cb4609a0af1c9175ed2fff605
Kashish Gupta <kashishgupta@kashish.local>
2015-12-14 14:47:17 +0530

    BugFix heroku serve static files

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 var ENV_PORT = process.env.PORT || 8080;
 var ENV_HOST = process.env.HOST || 'http://localhost';
+var CURRENT_DIR = __dirname;
+var STATIC_DIR_PATH = CURRENT_DIR + '/public';
 
-app.use(express.static('public'));
+console.log('DIR NAME ',STATIC_DIR_PATH);
+
+app.use(express.static(STATIC_DIR_PATH));
 
 app.listen(ENV_PORT,function(){
-  console.log('Server Listening on PORT:'+ENV_PORT);
+  console.log('Server Listening on '+ ENV_HOST +':'+ENV_PORT);
 });
