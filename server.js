4b2217d40a5ad9442f6bfe3a36b1a5be2aa6e998
Kashish Gupta <kashishgupta@kashish.local>
2015-12-14 14:36:52 +0530

    Added / route in application

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
+var ENV_PORT = process.env.PORT || 8080;
+var ENV_HOST = process.env.HOST || 'http://localhost';
 
-app.listen(8080,function(){
-  console.log('Server Listening on PORT:8080');
+app.get('/',function(req,res){
+  res.send('Hello World');
+});
+
+app.listen(ENV_PORT,function(){
+  console.log('Server Listening on PORT:'+ENV_PORT);
 });
