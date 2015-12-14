dfa07db047e1d98224c0bf0c01898a16a6430732
Kashish Gupta <kashishgupta@kashish.local>
2015-12-14 15:02:01 +0530

    added npm version

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var ENV_HOST = process.env.HOST || 'http://localhost';
 var CURRENT_DIR = __dirname;
 var STATIC_DIR_PATH = CURRENT_DIR + '/public';
 
-console.log('DIR NAME ',STATIC_DIR_PATH);
-
 app.use(express.static(STATIC_DIR_PATH));
 
 app.listen(ENV_PORT,function(){
