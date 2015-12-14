5841150289cd2ee5e3d52597f640346ed845cab5
Kashish Gupta <kashishgupta@kashish.local>
2015-12-14 14:41:59 +0530

    added static directory to server static pages

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var app = express();
 var ENV_PORT = process.env.PORT || 8080;
 var ENV_HOST = process.env.HOST || 'http://localhost';
 
-app.get('/',function(req,res){
-  res.send('Hello World');
-});
+app.use(express.static('public'));
 
 app.listen(ENV_PORT,function(){
   console.log('Server Listening on PORT:'+ENV_PORT);
