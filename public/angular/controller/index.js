6783e1ae7ff053c3053cf24194ad4c2f143b31c2
Kashish Gupta <kashishgupta@kashish.local>
2015-12-15 13:36:19 +0530

    angularjs image change

--- public/angular/controller/index.js
+++ public/angular/controller/index.js
@@ -1,5 +1,19 @@
 angular.module('propTiger',[])
 
-angular.module('propTiger').controller('IndexCtrl',['$scope',function($scope){
+angular.module('propTiger').controller('IndexCtrl',['$scope','$timeout',function($scope, $timeout){
     console.log('IndexCtrl');
+
+    $scope.bank = {
+      image:"assets/images/funny.jpg"
+    };
+
+
+    console.log($scope.bank.image);
+
+    $timeout(function(){
+      $scope.bank.image = "assets/images/buddha.jpg";
+      console.log($scope.bank.image);
+
+      $scope.$apply();
+    },500);
 }]);
